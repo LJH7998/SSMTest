c8fb7c57659a8d895d1146c8c553343c25c01da7
LJH7998 <genhao7998@163.com>
2022-07-04 00:36:14 +0800

    test2

---
+++ src/main/java/com/ljh/controller/BookController.java
@@ -0,0 +1,63 @@
+package com.ljh.controller;
+
+import com.ljh.pojo.Books;
+import com.ljh.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.awt.print.Book;
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+    @Autowired
+    @Qualifier("BookServiceImpl")
+    private BookService bookService;
+
+    @RequestMapping("/allBook")
+    public String list(Model model) {
+        List<Books> list = bookService.queryAllBook();
+        model.addAttribute("list", list);
+        return "allBook";
+    }
+
+    @RequestMapping("/toAddBook")
+    public String toAddBook() {
+        return "addBook";
+    }
+
+    @RequestMapping("/addBook")
+    public String addPaper(Books books) {
+        System.out.println(books);
+        bookService.addBook(books);
+        return "redirect:/book/allBook";
+    }
+
+    @RequestMapping("/toUpdateBook")
+    public String toUpdateBook(Model model, int id) {
+        Books books = bookService.queryBookById(id);
+        System.out.println(books);
+        model.addAttribute("book", books);
+        return "updateBook";
+    }
+
+    @RequestMapping("/updateBook")
+    public String updateBook(Model model, Books book) {
+        System.out.println(book);
+        bookService.updateBook(book);
+        Books books = bookService.queryBookById(book.getBookID());
+        model.addAttribute("books", books);
+        return "redirect:/book/allBook";
+    }
+
+    @RequestMapping("/del/{bookId}")
+    public String deleteBook(@PathVariable("bookId") int id) {
+        bookService.deleteBookById(id);
+        return "redirect:/book/allBook";
+    }
+}
